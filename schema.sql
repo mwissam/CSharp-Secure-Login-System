07c9ff1a6d5597110f12db380454622ee293998a
Mohammed Wissam <mwissam201@gmail.com>
2025-08-16 20:28:20 +0300

    Add database schema file (schema.sql)

---
+++ schema.sql
@@ -0,0 +1,19 @@
+-- This script creates the Users table for the CSharp-Secure-Login-System project.
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Users](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Username] [nvarchar](50) NOT NULL,
+	[Password] [nvarchar](MAX) NOT NULL, -- Note: This will store the HASHED password
+	[Name] [nvarchar](50) NOT NULL,
+	[Salt] [nvarchar](MAX) NOT NULL,
+	[Security] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
